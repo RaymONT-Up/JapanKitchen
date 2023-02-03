5b191e2a6a0d713c79c3d5d0ca7bdf6b09a733af
Raym0NT <konasr6@gmail.com>
2023-02-03 12:35:42 +0300

    added animation for cart button if we add item in cart

--- src/components/Header/HeaderCartButton/HeaderCartButton.jsx
+++ src/components/Header/HeaderCartButton/HeaderCartButton.jsx
@@ -1,16 +1,34 @@
 import styles from "./HeaderCartButton.module.css";
 import IconCart from "../../SvgIcons/IconCart/IconCart";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../../context/CartContext/CartContext";
 
 const HeaderCartButton = props => {
+  const [isButtonAnimated, setIsButtonAnimated] = useState(false);
+
   const cartContext = useContext(CartContext);
   const cartItemsNumber = cartContext.items.reduce((currentValue, item) => {
     return currentValue + item.amount;
   }, 0);
 
+  const buttonAnimatedClasses = `${styles.button} ${
+    isButtonAnimated ? styles.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (cartContext.items.length === 0) return;
+
+    setIsButtonAnimated(true);
+
+    const timer = setTimeout(() => setIsButtonAnimated(false), 300);
+    return () => {
+      clearTimeout(timer);
+    };
+    // Если мы добавляем сразу несколько элементов в корзину, то мы так удаляем пред. таймер и в случае если компонента будет удаленна, то так мы удалим все не нужные таймеры
+  }, [cartContext.items]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={buttonAnimatedClasses} onClick={props.onClick}>
       <span className={styles.icon}>
         <IconCart />
       </span>
